5204379cbb821407bb39e6e01596332512a34122
Bhaskar Pramanik <splashingbee@gmail.com>
2025-11-11 10:57:46 +0530

    add_junit_config_and_dummy_test

---
+++ src/test/java/io/github/bhaskarpramanik/automation/tests/DemoJunitTest.java
@@ -0,0 +1,15 @@
+package io.github.bhaskarpramanik.automation.tests;
+
+import io.github.bhaskarpramanik.automation.base.config.junit.JunitConfiguration;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@JunitConfiguration
+class DemoJunitTest {
+
+  @Test
+  void demoTest() {
+    // dummy junit test
+    Assertions.assertThat(true).isNotNull();
+  }
+}
